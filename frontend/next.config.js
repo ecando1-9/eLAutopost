d534866fe4e2d3a4f895c2e16bbb3167b5af2c52
ecando1-9 <ecando976@gmail.com>
2026-03-26 23:33:29 +0530

    fix: removed local proxy rewrite causing Vercel DNS crash

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -49,14 +49,6 @@ const nextConfig = {
             },
         ]
     },
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://127.0.0.1:8000/api/:path*',
-            },
-        ]
-    },
 }
 
 module.exports = nextConfig
